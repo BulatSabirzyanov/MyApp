4572402911ef20bbf61054e976bd10e5b64f10cf
sabir <sabirzyanov429@gmail.com>
2022-12-06 22:13:48 +0300

    hm8

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,25 +1,38 @@
 package com.example.myapplication
 
+import android.Manifest
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+import androidx.core.app.ActivityCompat
+import com.example.myapplication.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity(){
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.mainBottomNavigationView)
-        val navController = findNavController(R.id.fragment)
-        //val appBarConfiguration = AppBarConfiguration(setOf(R.id.newsFragment,R.id.likesFragment,R.id.settingsFragment))
-        bottomNavigationView.setupWithNavController(navController)
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            if(destination.id == R.id.settings2Fragment) {
-                bottomNavigationView.visibility = View.GONE
-            } else {
-                bottomNavigationView.visibility = View.VISIBLE
+        setContentView(binding.root)
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            0
+        )
+        binding.btnStart.setOnClickListener {
+            Intent(applicationContext, LocationService::class.java).apply {
+                action = LocationService.ACTION_START
+                startService(this)
+            }
+        }
+        binding.btnStop.setOnClickListener{
+            Intent(applicationContext, LocationService::class.java).apply {
+                action = LocationService.ACTION_STOP
+                startService(this)
             }
         }
 
